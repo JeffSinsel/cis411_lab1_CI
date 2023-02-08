e0e755bd869c8b1c516636f006e0ff776b84fb26
JeffSinsel <jeffsinsel@gmail.com>
2023-02-07 22:25:41 -0500

    update

--- script.sh
+++ script.sh
@@ -3,9 +3,8 @@
 #put your docker login below where jeffsinsel is 
 DOCKER_LOGIN='jeffsinsel'
 
-sudo service docker start
 docker pull ${DOCKER_LOGIN}/lab1
-if docker ps -aq; then
+if ! [ $(docker ps -aq) == "" ]; then
     docker stop $(docker ps -aq)
 fi
 docker run -p 4000:4000 --rm ${DOCKER_LOGIN}/lab1
--- server.js
+++ server.js
@@ -131,5 +131,6 @@ app.use('/graphql', express_graphql({
 }))
 
 app.listen(4000, () => {
-    console.log('Express GraphQL Server Now Running On http://AWS_DOMAIN:4000/graphql');
+    console.log('Express GraphQL Server Now Running On http://AWS_DOMAIN:4000/graphql')
+    return 'exit 0'
 })
